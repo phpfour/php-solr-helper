25f8e5c3b14026ebd27caa148defe47c95b0ed20
Emran Hasan <phpfour@gmail.com>
2012-07-27 10:00:07 +0600

    Removed the hard dependency of the ApacheSolr library.

--- examples/full.php
+++ examples/full.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../vendor/SolrPhpClient/Apache/Solr/Service.php';
-require_once __DIR__ . '/../src/Emran/SolrHelper.php';
-
 $solrHelper = new \Emran\SolrHelper(new \Apache_Solr_Service('localhost', '8080', '/solr/'));
 
 $result = $solrHelper->addSingleValueCriteria('make', 'BMW')
